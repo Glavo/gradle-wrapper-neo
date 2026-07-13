ffeac3a835be495976795da49fe893505dfea0aa
Glavo <zjx001202@gmail.com>
2026-07-13 23:54:34 +0800

    Bump version to 0.3.0-SNAPSHOT in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.glavo"
-version = providers.gradleProperty("version").getOrElse("0.2.0")
+version = providers.gradleProperty("version").getOrElse("0.3.0-SNAPSHOT")
 
 allprojects {
     group = rootProject.group
